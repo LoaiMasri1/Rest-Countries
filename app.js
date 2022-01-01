fa614158b314745c34d69438a9dd7c77400f4ead
LoaiMasri <s12010098@stu.najah.edu>
2022-01-01 17:37:25 +0200

    search for city

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-let btn=document.querySelector('#toggle_btn')
-let label = document.querySelector('.dark_mode p')
-let container=document.querySelector('.flag_container')
-btn.addEventListener('click', () => {
-   document.body.classList.toggle('dark')
-   label.textContent='Light Mode';
+let btn = document.querySelector("#toggle_btn");
+let label = document.querySelector(".dark_mode p");
+let container = document.querySelector(".flag_container");
+let textBox = document.querySelector("#text");
+btn.addEventListener("click", darkMode);
+window.addEventListener("load", init);
+
+textBox.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    let value = textBox.textContent;
+    search(value);
+  }
 });
 
-
-async function hello(){
-    let curl='https://restcountries.com/v3.1/all'
-    let response=await fetch(curl)
-    let data =await response.json()
-    console.log(data[0].population);
-    data.forEach(city => {
-    let div=document.createElement('div')
-        div.innerHTML=`<div class="card">
+async function init() {
+  let curl = "https://restcountries.com/v3.1/all";
+  let response = await fetch(curl);
+  let data = await response.json();
+  console.log(data[0].population);
+  data.forEach((city) => {
+    let div = document.createElement("div");
+    div.innerHTML = `<div class="card">
         <div class="card_img"><img src="${city.flags.png}">
     </div>
     <div class="card_content">
@@ -25,7 +31,37 @@ async function hello(){
         </p>
     </div>
 </div>`;
-container.appendChild(div)
-    });
+    container.appendChild(div);
+  });
 }
-hello()
+
+async function search(CITY) {
+  try {
+    let curl = `https://restcountries.com/v2/name/${CITY}`;
+    let response = await fetch(curl);
+    let data = await response.json();
+    container.innerHTML = " ";
+    data.forEach((city) => {
+      let div = document.createElement("div");
+      div.innerHTML = `<div class="card">
+            <div class="card_img"><img src="${city.flags.png}">
+        </div>
+        <div class="card_content">
+            <h3 class="card_title">${city.name.common}</h3>
+            <p>Population : <span class="population">${city.population}</span><br>
+                Region : <span class="region">${city.region}</span><br>
+                Capital : <span class="capital">${city.capital}</span>
+            </p>
+        </div>
+    </div>`;
+      container.appendChild(div);
+    });
+  } catch (error) {
+    alert(error);
+  }
+}
+
+function darkMode() {
+  document.body.classList.toggle("dark");
+  label.textContent = "Light Mode";
+}
