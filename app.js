e26aa0217428dfb7fc04f4e5ccf4730ee8bbfe89
LoaiMasri <s12010098@stu.najah.edu>
2022-01-01 19:00:55 +0200

    not important

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-let btn = document.querySelector("#toggle_btn");
-let label = document.querySelector(".dark_mode p");
-let container = document.querySelector(".flag_container");
-let textBox = document.querySelector("#text");
-let select = document.querySelectorAll("#select");
-topBtn = document.getElementById("toTop");
+let btn = document.querySelector("#toggle_btn")
+let label = document.querySelector(".dark_mode p")
+let container = document.querySelector(".flag_container")
+let textBox = document.querySelector("#text")
+let select = document.querySelectorAll("#select")
+let topBtn = document.getElementById("toTop")
+let searchBtn=document.querySelector('#search_btn')
 
-window.addEventListener("load", init);
-window.addEventListener("load", filter);
-window.addEventListener("scroll", scrollFunction);
-btn.addEventListener("click", darkMode);
-textBox.addEventListener("keyup", hitEnter);
-topBtn.addEventListener("click", topFunction);
+window.addEventListener("load", init)
+window.addEventListener("load", filter)
+window.addEventListener("scroll", scrollFunction)
+btn.addEventListener("click", darkMode)
+textBox.addEventListener("keyup", hitEnter)
+searchBtn.addEventListener('click',searchFun)
+topBtn.addEventListener("click", topFunction)
 
 async function init() {
   try {
-    let curl = "https://restcountries.com/v3.1/all";
-    let response = await fetch(curl);
-    let data = await response.json();
-    container.innerHTML = " ";
+    let curl = "https://restcountries.com/v3.1/all"
+    let response = await fetch(curl)
+    let data = await response.json()
+    container.innerHTML = " "
     data.forEach((city) => {
-      let div = document.createElement("div");
+      let div = document.createElement("div")
       div.innerHTML = `<div class="card">
-          <div class="card_img"><img src="${city.flags.png}">
+          <div class="card_img"><img src="${city.flags.png}" alt='${city.name.common} Flag'>
       </div>
       <div class="card_content">
           <h3 class="card_title">${city.name.common}</h3>
@@ -30,24 +32,25 @@ async function init() {
               Capital : <span class="capital">${city.capital}</span>
           </p>
       </div>
-  </div>`;
-      container.appendChild(div);
-    });
+  </div>`
+      container.appendChild(div)
+    })
   } catch (error) {
-    alert(error);
+    console.error(error)
+    alert('Plese Enter A Valid Country')
   }
 }
 
 async function search(name, CITY) {
   try {
-    let curl = `https://restcountries.com/v2/${name}/${CITY}`;
-    let response = await fetch(curl);
-    let data = await response.json();
-    container.innerHTML = " ";
+    let curl = `https://restcountries.com/v2/${name}/${CITY}`
+    let response = await fetch(curl)
+    let data = await response.json()
+    container.innerHTML = " "
     data.forEach((city) => {
-      let div = document.createElement("div");
+      let div = document.createElement("div")
       div.innerHTML = `<div class="card">
-            <div class="card_img"><img src="${city.flags.png}">
+            <div class="card_img"><img src="${city.flags.png}" alt="${city.name} Flag">
         </div>
         <div class="card_content">
             <h3 class="card_title">${city.name}</h3>
@@ -56,11 +59,13 @@ async function search(name, CITY) {
                 Capital : <span class="capital">${city.capital}</span>
             </p>
         </div>
-    </div>`;
-      container.appendChild(div);
-    });
+    </div>`
+      container.appendChild(div)
+    })
   } catch (error) {
-    alert(error);
+      console.error(error)
+    alert('Plese Enter A Valid Country')
+
   }
 }
 
@@ -68,27 +73,34 @@ function filter() {
   select.forEach((option) => {
     option.addEventListener("change", () => {
       if (option.value === "All") {
-        init();
+        init()
       } else {
-        let value = option.value;
-        search("region", value);
+        let value = option.value
+        search("region", value)
       }
-    });
-  });
+    })
+  })
 }
 
 function darkMode() {
-  document.body.classList.toggle("dark");
-  label.textContent = "Light Mode";
+  document.body.classList.toggle("dark")
+  label.textContent = "Light Mode"
 }
 
 function hitEnter(event) {
   if (event.keyCode === 13) {
-    let value = textBox.value;
-    textBox.value = "";
-    search("name", value);
-    event.preventDefault();
+    let value = textBox.value
+    textBox.value = ""
+    search("name", value)
+    event.preventDefault()
   }
+}
+
+function searchFun(event){
+    event.preventDefault()
+    let value = textBox.value
+    textBox.value = ""
+    search("name", value)
 }
 
 function scrollFunction() {
@@ -96,13 +108,13 @@ function scrollFunction() {
     document.body.scrollTop > 500 ||
     document.documentElement.scrollTop > 500
   ) {
-    topBtn.style.bottom = "30px";
+    topBtn.style.bottom = "30px"
   } else {
-    topBtn.style.bottom = "-100px";
+    topBtn.style.bottom = "-100px"
   }
 }
 
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
 }
